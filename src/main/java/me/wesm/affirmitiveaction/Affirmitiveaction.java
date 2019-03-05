ca2d53004d131fa8ea03abc459978c1f6fb89d6a
BuildTools <unconfigured@null.spigotmc.org>
2019-03-05 16:26:13 -0500

    added chat message notifying players of the victim's unacceptable behavior

--- src/main/java/me/wesm/affirmitiveaction/Affirmitiveaction.java
+++ src/main/java/me/wesm/affirmitiveaction/Affirmitiveaction.java
@@ -1,18 +1,20 @@
 package me.wesm.affirmitiveaction;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.Server;
+import static org.bukkit.Bukkit.broadcastMessage;
 
 public final class Affirmitiveaction extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("[AffirmitiveAction]: annhiliating discriminatory entities");
+        System.out.println("[AffirmitiveAction]: annhiliating discriminatory entities.");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -23,6 +25,7 @@ public final class Affirmitiveaction extends JavaPlugin implements Listener {
         if (msg.contains("nigger")){
             player.setHealth(0.01);
             player.getWorld().strikeLightning(player.getLocation());
+            broadcastMessage(ChatColor.DARK_RED + player.getName() + " was vaporized for discriminatory language.");
         }
     }
 
